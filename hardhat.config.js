bd98a23736e8145b2629aa3927bcb54356ebc9b0
Franco Perez <franco.perez03@gmail.com>
2022-12-26 17:45:24 -0300

    create unit test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox')
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan')
+require('./tasks/block-number')
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -13,6 +14,10 @@ module.exports = {
 			url: GOERLI_RPC_URL,
 			accounts: [PRIVATE_KEY],
 			chainId: 5,
+		},
+		localhost: {
+			url: 'http://127.0.0.1:8545/',
+			chainId: 31337, //same as hardhat
 		},
 	},
 	etherscan: {
