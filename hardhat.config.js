4d6d4a35369a56ba91fd492a9872322198cb08a7
Franco Perez <franco.perez03@gmail.com>
2022-12-26 18:28:16 -0300

    added gas-report and coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,18 @@ require('@nomicfoundation/hardhat-toolbox')
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan')
 require('./tasks/block-number')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || 'https://eth-goerli'
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	defaultNetwork: 'hardhat',
+	solidity: '0.8.7',
 	networks: {
 		goerli: {
 			url: GOERLI_RPC_URL,
@@ -23,5 +28,12 @@ module.exports = {
 	etherscan: {
 		apiKey: ETHERSCAN_API_KEY,
 	},
-	solidity: '0.8.7',
+	gasReporter: {
+		enabled: false,
+		outputFile: 'gas-report.txt',
+		noColors: true,
+		currency: 'USD',
+		coinmarketcap: COINMARKETCAP_API_KEY,
+		token: 'MATIC',
+	},
 }
